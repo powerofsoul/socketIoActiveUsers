1f6194bd79e666caca16aa4c09fcd28136028971
Florin Munteanu <florinmunteanu96@gmail.com>
2019-09-14 18:11:36 +0300

    Add initial project

---
+++ main.js
@@ -0,0 +1,37 @@
+const _ = require('underscore');
+
+const server = require('http').createServer();
+const pages = {
+};
+
+const io = require('socket.io')(server, {
+  path: '',
+  serveClient: false,
+  // below are engine.IO options
+  pingInterval: 10000,
+  pingTimeout: 5000,
+  cookie: false
+});
+
+const activeUsers = io.of('/activeusers');
+
+activeUsers.on('connect', (socket) => {
+    const uri = socket.request.headers.referer;
+    const address = socket.handshake.address;
+
+    if(pages[uri]){
+        pages[uri].push(address)
+    }else{
+        pages[uri] = [address];
+    }
+
+    const activeUsersCount = _.uniq(pages[uri]) - 1;
+
+    socket.emit('stateChanged', {connectedUsers: activeUsersCount});
+
+    socket.on('disconnect', () => {
+        pages[uri] = _.without(pages[uri], address);
+    });
+});
+
+server.listen(3000);
